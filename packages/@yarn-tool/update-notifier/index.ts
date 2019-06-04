601717b9b8e5c1fe98d90b7cee560888176ba3af
bluelovers <codelovers@users.sourceforge.net>
2019-06-04 20:15:07 +0800

    publish new version

--- packages/@yarn-tool/update-notifier/index.ts
+++ packages/@yarn-tool/update-notifier/index.ts
@@ -4,7 +4,7 @@ export function updateNotifier(__dirname: string, force?: boolean)
 	if (force || /node_modules/i.test(__dirname) || !require('is-npx')())
 	{
 		const _updateNotifier = require('update-notifier');
-		const pkg = require(require('path').join(__dirname, '../package.json'));
+		const pkg = require(require('path').join(__dirname, 'package.json'));
 		return _updateNotifier({ pkg }).notify({
 			shouldNotifyInNpmScript: true,
 		});
