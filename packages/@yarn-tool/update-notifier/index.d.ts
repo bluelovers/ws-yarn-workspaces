4316697acc6b7782b3a70bd8c65873191e15dcdd
bluelovers <codelovers@users.sourceforge.net>
2019-08-19 23:17:18 +0800

    publish new version

--- packages/@yarn-tool/update-notifier/index.d.ts
+++ packages/@yarn-tool/update-notifier/index.d.ts
@@ -1,2 +1,4 @@
-export declare function updateNotifier(__dirname: string, force?: boolean): void;
+export declare type IUpdateNotifier = typeof import('update-notifier');
+export declare function notNpxMaybe(__dirname: string): boolean;
+export declare function updateNotifier(__dirname: string, force?: boolean): IUpdateNotifier;
 export default updateNotifier;
--- packages/@yarn-tool/update-notifier/index.ts
+++ packages/@yarn-tool/update-notifier/index.ts
@@ -1,17 +1,26 @@
 
-export function updateNotifier(__dirname: string, force?: boolean): void
+export type IUpdateNotifier = typeof import('update-notifier');
+
+export function notNpxMaybe(__dirname: string)
 {
-	if (force || /node_modules/i.test(__dirname) || !require('@yarn-tool/is-npx').isNpx({
+	return __dirname && /node_modules/i.test(__dirname) || !require('@yarn-tool/is-npx').isNpx({
 		__dirname,
-	}))
+	})
+}
+
+export function updateNotifier(__dirname: string, force?: boolean): IUpdateNotifier
+{
+	if (force || notNpxMaybe(__dirname))
 	{
-		const _updateNotifier = require('update-notifier') as typeof import('update-notifier');
+		const _updateNotifier = require('update-notifier') as IUpdateNotifier;
 		const pkg = require(require('path').join(__dirname, 'package.json'));
 		return _updateNotifier({ pkg }).notify({
 			// @ts-ignore
 			shouldNotifyInNpmScript: true,
 		});
 	}
+
+	return null
 }
 
 export default updateNotifier
