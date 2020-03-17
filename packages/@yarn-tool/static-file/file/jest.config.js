ffde5eee91e7e793dc31f1583dc14ccee4f79c21
bluelovers <codelovers@users.sourceforge.net>
2020-03-17 10:46:48 +0800

    .

--- packages/@yarn-tool/static-file/file/jest.config.js
+++ packages/@yarn-tool/static-file/file/jest.config.js
@@ -4,7 +4,7 @@ module.exports = {
 	testEnvironment: 'node',
 	//testMatch: ['**/*.test.ts', '**/*.spec.ts'],
 	testRegex: ['\\.(test|spec)\\.(ts|tsx)$'],
-	testRunner: 'jest-circus/runner',
+	//testRunner: 'jest-circus/runner',
 	transform: {
 		'^.+\\.ts$': 'ts-jest',
 	},
