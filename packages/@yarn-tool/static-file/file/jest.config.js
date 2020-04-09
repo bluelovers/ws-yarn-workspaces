a65620b0f4edb986f997652cf123f0442ea7bfa8
bluelovers <codelovers@users.sourceforge.net>
2020-04-10 07:04:54 +0800

    feat: add match tests

--- packages/@yarn-tool/static-file/file/jest.config.js
+++ packages/@yarn-tool/static-file/file/jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
 	moduleFileExtensions: ['ts', 'js'],
 	testEnvironment: 'node',
 	//testMatch: ['**/*.test.ts', '**/*.spec.ts'],
-	testRegex: ['\\.(test|spec)\\.(ts|tsx)$'],
+	testRegex: ['\\.(tests?|spec)\\.(ts|tsx)$'],
 	//testRunner: 'jest-circus/runner',
 	transform: {
 		'^.+\\.ts$': 'ts-jest',
