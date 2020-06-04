4cc76d103ac77a23e6b2870c32ae287cece94557
bluelovers <codelovers@users.sourceforge.net>
2020-06-05 01:34:27 +0800

    build: .js

--- packages/node-package-json-loader/index.js
+++ packages/node-package-json-loader/index.js
@@ -19,179 +19,176 @@ const pkg_up_1 = __importDefault(require("pkg-up"));
 const bind_decorator_1 = __importDefault(require("bind-decorator"));
 const util_1 = require("./util");
 const path_1 = __importDefault(require("path"));
-let PackageJsonLoader = /** @class */ (() => {
-    class PackageJsonLoader {
-        constructor(fileOrJson, ...argv) {
-            this._use = [];
-            if (typeof fileOrJson === 'string') {
-                this.setFilename(fileOrJson);
-            }
-            else if (Buffer.isBuffer(fileOrJson)) {
-                this.setJson(JSON.parse(fileOrJson.toString()));
-            }
-            else if (typeof fileOrJson === 'object') {
-                this.setJson(JSON.parse(fileOrJson.toString()));
-            }
-            else if (fileOrJson != null) {
-                throw new TypeError(`fileOrJson is not valid`);
-            }
+class PackageJsonLoader {
+    constructor(fileOrJson, ...argv) {
+        this._use = [];
+        if (typeof fileOrJson === 'string') {
+            this.setFilename(fileOrJson);
         }
-        static create(file, ...argv) {
-            return new this(file, ...argv);
+        else if (Buffer.isBuffer(fileOrJson)) {
+            this.setJson(JSON.parse(fileOrJson.toString()));
         }
-        static createByJson(json, ...argv) {
-            return new this(json, ...argv);
+        else if (typeof fileOrJson === 'object') {
+            this.setJson(JSON.parse(fileOrJson.toString()));
         }
-        static findPackageJsonPath(name) {
-            return pkg_up_1.default.sync({
-                cwd: require.resolve(name),
-            });
+        else if (fileOrJson != null) {
+            throw new TypeError(`fileOrJson is not valid`);
         }
-        static loadByModuleName(name) {
-            let file = this.findPackageJsonPath(name);
-            let pkg = this.create(file);
-            if (pkg.data.name !== name) {
-                throw new TypeError(`package name not match, '${pkg.data.name}' != '${name}'`);
-            }
-            return pkg;
+    }
+    static create(file, ...argv) {
+        return new this(file, ...argv);
+    }
+    static createByJson(json, ...argv) {
+        return new this(json, ...argv);
+    }
+    static findPackageJsonPath(name) {
+        return pkg_up_1.default.sync({
+            cwd: require.resolve(name),
+        });
+    }
+    static loadByModuleName(name) {
+        let file = this.findPackageJsonPath(name);
+        let pkg = this.create(file);
+        if (pkg.data.name !== name) {
+            throw new TypeError(`package name not match, '${pkg.data.name}' != '${name}'`);
         }
-        use(ls) {
-            if (Array.isArray(ls)) {
-                this._use.push(...ls);
-            }
-            else {
-                this._use.push(ls);
-            }
+        return pkg;
+    }
+    use(ls) {
+        if (Array.isArray(ls)) {
+            this._use.push(...ls);
         }
-        setFilename(file) {
-            // @ts-ignore
-            this.file = file;
-            return this;
+        else {
+            this._use.push(ls);
         }
-        setJson(json) {
-            this.loaded = true;
-            this.json = json;
-            return this;
+    }
+    setFilename(file) {
+        // @ts-ignore
+        this.file = file;
+        return this;
+    }
+    setJson(json) {
+        this.loaded = true;
+        this.json = json;
+        return this;
+    }
+    read(reload) {
+        if (!this.loaded || reload) {
+            this.json = fs_extra_1.default.readJSONSync(this.file);
         }
-        read(reload) {
-            if (!this.loaded || reload) {
-                this.json = fs_extra_1.default.readJSONSync(this.file);
-            }
-            this.loaded = true;
-            return this;
+        this.loaded = true;
+        return this;
+    }
+    get dir() {
+        return path_1.default.dirname(this.file);
+    }
+    /**
+     * skip typescript type check
+     */
+    get unsafeTypeData() {
+        return this.data;
+    }
+    /**
+     * skip typescript type check
+     */
+    set unsafeTypeData(json) {
+        this.data = json;
+    }
+    set data(json) {
+        this.overwrite(json);
+    }
+    get data() {
+        if (!this.loaded && this.file) {
+            this.read();
         }
-        get dir() {
-            return path_1.default.dirname(this.file);
-        }
-        /**
-         * skip typescript type check
-         */
-        get unsafeTypeData() {
-            return this.data;
-        }
-        /**
-         * skip typescript type check
-         */
-        set unsafeTypeData(json) {
-            this.data = json;
-        }
-        set data(json) {
-            this.overwrite(json);
-        }
-        get data() {
-            if (!this.loaded && this.file) {
-                this.read();
-            }
-            return this.json;
-        }
-        overwrite(json) {
-            this.loaded = true;
-            this.json = json;
-            return this;
-        }
-        autofix() {
-            let self = this;
-            let dir;
-            if (self.file && fs_extra_1.default.existsSync(dir = self.dir)) {
-                if (self.data) {
-                    if (self.data.bin) {
-                        if (typeof self.data.bin === 'string') {
-                            let bin_new = util_1.fixBinPath(self.data.bin, dir);
-                            if (bin_new) {
-                                self.data.bin = bin_new;
-                            }
+        return this.json;
+    }
+    overwrite(json) {
+        this.loaded = true;
+        this.json = json;
+        return this;
+    }
+    autofix() {
+        let self = this;
+        let dir;
+        if (self.file && fs_extra_1.default.existsSync(dir = self.dir)) {
+            if (self.data) {
+                if (self.data.bin) {
+                    if (typeof self.data.bin === 'string') {
+                        let bin_new = util_1.fixBinPath(self.data.bin, dir);
+                        if (bin_new) {
+                            self.data.bin = bin_new;
                         }
-                        else if (typeof self.data.bin === 'object' && !Array.isArray(self.data.bin)) {
-                            Object.keys(self.data.bin)
-                                .forEach(function (key) {
-                                if (typeof self.data.bin[key] === 'string') {
-                                    let bin_new = util_1.fixBinPath(self.data.bin[key], dir);
-                                    if (bin_new) {
-                                        self.data.bin[key] = bin_new;
-                                    }
+                    }
+                    else if (typeof self.data.bin === 'object' && !Array.isArray(self.data.bin)) {
+                        Object.keys(self.data.bin)
+                            .forEach(function (key) {
+                            if (typeof self.data.bin[key] === 'string') {
+                                let bin_new = util_1.fixBinPath(self.data.bin[key], dir);
+                                if (bin_new) {
+                                    self.data.bin[key] = bin_new;
                                 }
-                            });
-                        }
+                            }
+                        });
                     }
-                    if (!self.data.publishConfig
-                        && self.data.name
-                        && /\//.test(self.data.name)
-                        && !self.data.private) {
-                        self.data.publishConfig = {
-                            access: "public",
-                        };
-                    }
+                }
+                if (!self.data.publishConfig
+                    && self.data.name
+                    && /\//.test(self.data.name)
+                    && !self.data.private) {
+                    self.data.publishConfig = {
+                        access: "public",
+                    };
                 }
             }
         }
-        run(options = {}) {
-            if (options.autofix == null || options.autofix) {
-                this.autofix();
-            }
-            this._use.forEach(fn => fn.call(this, this.data));
-            return this;
-        }
-        exists() {
-            return fs_extra_1.default.existsSync(this.file);
-        }
-        stringify() {
-            return JSON.stringify(this.json, null, 2);
-        }
-        sort() {
-            if (typeof this.data === 'undefined' || this.data === null) {
-                throw new Error(`data is undefined`);
-            }
-            this.data = sort_package_json3_1.default(this.data);
-            return this;
-        }
-        write() {
-            if (!this.file) {
-                throw new Error(`file is undefined`);
-            }
-            fs_extra_1.default.writeFileSync(this.file, this.stringify());
-            return this;
-        }
-        writeOnlyWhenLoaded() {
-            if (this.loaded) {
-                this.write();
-            }
-            return this.loaded;
-        }
     }
-    __decorate([
-        bind_decorator_1.default,
-        __metadata("design:type", Function),
-        __metadata("design:paramtypes", [Object, Object]),
-        __metadata("design:returntype", void 0)
-    ], PackageJsonLoader, "create", null);
-    __decorate([
-        bind_decorator_1.default,
-        __metadata("design:type", Function),
-        __metadata("design:paramtypes", [String]),
-        __metadata("design:returntype", void 0)
-    ], PackageJsonLoader, "loadByModuleName", null);
-    return PackageJsonLoader;
-})();
+    run(options = {}) {
+        if (options.autofix == null || options.autofix) {
+            this.autofix();
+        }
+        this._use.forEach(fn => fn.call(this, this.data));
+        return this;
+    }
+    exists() {
+        return fs_extra_1.default.existsSync(this.file);
+    }
+    stringify() {
+        return JSON.stringify(this.json, null, 2);
+    }
+    sort() {
+        if (typeof this.data === 'undefined' || this.data === null) {
+            throw new Error(`data is undefined`);
+        }
+        this.data = sort_package_json3_1.default(this.data);
+        return this;
+    }
+    write() {
+        if (!this.file) {
+            throw new Error(`file is undefined`);
+        }
+        fs_extra_1.default.writeFileSync(this.file, this.stringify());
+        return this;
+    }
+    writeOnlyWhenLoaded() {
+        if (this.loaded) {
+            this.write();
+        }
+        return this.loaded;
+    }
+}
+__decorate([
+    bind_decorator_1.default,
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [Object, Object]),
+    __metadata("design:returntype", void 0)
+], PackageJsonLoader, "create", null);
+__decorate([
+    bind_decorator_1.default,
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [String]),
+    __metadata("design:returntype", void 0)
+], PackageJsonLoader, "loadByModuleName", null);
 exports.PackageJsonLoader = PackageJsonLoader;
 exports.default = PackageJsonLoader;
 //# sourceMappingURL=index.js.map
--- packages/yarn-cmd-module/index.js
+++ packages/yarn-cmd-module/index.js
@@ -11,150 +11,147 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
+var _a, _b;
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.YargsCommandModule = exports.SYM_PROP = exports.SYM_YARGS = exports.SYM_CONFIG = void 0;
 const core_decorators_1 = require("core-decorators");
 exports.SYM_CONFIG = Symbol.for('config');
 exports.SYM_YARGS = Symbol.for('yargs');
 exports.SYM_PROP = Symbol.for('fakeProp');
-let YargsCommandModule = /** @class */ (() => {
-    var _a, _b;
-    class YargsCommandModule {
-        constructor(config) {
-            this[_a] = {};
-            this[_b] = {};
-            this[exports.SYM_CONFIG] = config;
-        }
-        static create(config) {
-            return new this(config);
-        }
-        /** string (or array of strings) that executes this command when given on the command line, first string may contain positional args */
-        get command() {
-            return this[exports.SYM_CONFIG].command;
-        }
-        set command(value) {
-            this[exports.SYM_CONFIG].command = value;
-        }
-        /** string used as the description for the command in help text, use `false` for a hidden command */
-        get describe() {
-            return this[exports.SYM_CONFIG].describe;
-        }
-        set describe(value) {
-            this[exports.SYM_CONFIG].describe = value;
-        }
-        /** array of strings (or a single string) representing aliases of `exports.command`, positional args defined in an alias are ignored */
-        get aliases() {
-            return this[exports.SYM_CONFIG].aliases;
-        }
-        set aliases(value) {
-            this[exports.SYM_CONFIG].aliases = value;
-        }
-        /** object declaring the options the command accepts, or a function accepting and returning a yargs instance */
-        get builder() {
-            // @ts-ignore
-            let builder = this[exports.SYM_CONFIG].builder;
-            if (typeof builder == 'function') {
-                let self = this;
-                return function builder(yargs) {
-                    self[exports.SYM_YARGS].from = yargs;
-                    // @ts-ignore
-                    return self[exports.SYM_YARGS].to = builder.call(this, yargs);
-                };
-            }
-            // @ts-ignore
-            return this[exports.SYM_CONFIG].builder;
-        }
-        get yargs() {
-            return this[exports.SYM_YARGS].to;
-        }
-        /** a function which will be passed the parsed argv. */
-        get handler() {
-            // @ts-ignore
-            return this.newHandler(this[exports.SYM_CONFIG].handler);
-        }
-        setHandler(cb) {
-            // @ts-ignore
-            this[exports.SYM_CONFIG].handler = cb;
-            return this;
-        }
-        newHandler(cb) {
-            let self = this;
-            return (args) => {
-                return cb(args, self[exports.SYM_YARGS].to, self);
-            };
-        }
-        toValue() {
-            return this[Symbol.toPrimitive]();
-        }
-        [(_a = exports.SYM_CONFIG, _b = exports.SYM_YARGS, Symbol.toPrimitive)]() {
-            let { command, describe, aliases } = this;
-            if (Array.isArray(command)) {
-                command = command.slice();
-            }
-            if (Array.isArray(aliases)) {
-                aliases = aliases.slice();
-            }
-            let builder = this.builder;
-            let handler = this.handler;
-            return {
-                command,
-                aliases,
-                describe,
-                builder,
-                handler,
-            };
-        }
+class YargsCommandModule {
+    constructor(config) {
+        this[_a] = {};
+        this[_b] = {};
+        this[exports.SYM_CONFIG] = config;
     }
-    __decorate([
-        core_decorators_1.autobind,
-        __metadata("design:type", Object),
-        __metadata("design:paramtypes", [Object])
-    ], YargsCommandModule.prototype, "command", null);
-    __decorate([
-        core_decorators_1.autobind,
-        __metadata("design:type", Object),
-        __metadata("design:paramtypes", [Object])
-    ], YargsCommandModule.prototype, "describe", null);
-    __decorate([
-        core_decorators_1.autobind,
-        __metadata("design:type", Object),
-        __metadata("design:paramtypes", [Object])
-    ], YargsCommandModule.prototype, "aliases", null);
-    __decorate([
-        core_decorators_1.autobind,
-        __metadata("design:type", Object),
-        __metadata("design:paramtypes", [])
-    ], YargsCommandModule.prototype, "builder", null);
-    __decorate([
-        core_decorators_1.autobind,
-        __metadata("design:type", Object),
-        __metadata("design:paramtypes", [])
-    ], YargsCommandModule.prototype, "yargs", null);
-    __decorate([
-        core_decorators_1.autobind,
-        __metadata("design:type", Function),
-        __metadata("design:paramtypes", [])
-    ], YargsCommandModule.prototype, "handler", null);
-    __decorate([
-        core_decorators_1.autobind,
-        __metadata("design:type", Function),
-        __metadata("design:paramtypes", [Function]),
-        __metadata("design:returntype", void 0)
-    ], YargsCommandModule.prototype, "setHandler", null);
-    __decorate([
-        core_decorators_1.autobind,
-        __metadata("design:type", Function),
-        __metadata("design:paramtypes", [Function]),
-        __metadata("design:returntype", void 0)
-    ], YargsCommandModule.prototype, "newHandler", null);
-    __decorate([
-        core_decorators_1.autobind,
-        __metadata("design:type", Function),
-        __metadata("design:paramtypes", []),
-        __metadata("design:returntype", void 0)
-    ], YargsCommandModule.prototype, "toValue", null);
-    return YargsCommandModule;
-})();
+    static create(config) {
+        return new this(config);
+    }
+    /** string (or array of strings) that executes this command when given on the command line, first string may contain positional args */
+    get command() {
+        return this[exports.SYM_CONFIG].command;
+    }
+    set command(value) {
+        this[exports.SYM_CONFIG].command = value;
+    }
+    /** string used as the description for the command in help text, use `false` for a hidden command */
+    get describe() {
+        return this[exports.SYM_CONFIG].describe;
+    }
+    set describe(value) {
+        this[exports.SYM_CONFIG].describe = value;
+    }
+    /** array of strings (or a single string) representing aliases of `exports.command`, positional args defined in an alias are ignored */
+    get aliases() {
+        return this[exports.SYM_CONFIG].aliases;
+    }
+    set aliases(value) {
+        this[exports.SYM_CONFIG].aliases = value;
+    }
+    /** object declaring the options the command accepts, or a function accepting and returning a yargs instance */
+    get builder() {
+        // @ts-ignore
+        let builder = this[exports.SYM_CONFIG].builder;
+        if (typeof builder == 'function') {
+            let self = this;
+            return function builder(yargs) {
+                self[exports.SYM_YARGS].from = yargs;
+                // @ts-ignore
+                return self[exports.SYM_YARGS].to = builder.call(this, yargs);
+            };
+        }
+        // @ts-ignore
+        return this[exports.SYM_CONFIG].builder;
+    }
+    get yargs() {
+        return this[exports.SYM_YARGS].to;
+    }
+    /** a function which will be passed the parsed argv. */
+    get handler() {
+        // @ts-ignore
+        return this.newHandler(this[exports.SYM_CONFIG].handler);
+    }
+    setHandler(cb) {
+        // @ts-ignore
+        this[exports.SYM_CONFIG].handler = cb;
+        return this;
+    }
+    newHandler(cb) {
+        let self = this;
+        return (args) => {
+            return cb(args, self[exports.SYM_YARGS].to, self);
+        };
+    }
+    toValue() {
+        return this[Symbol.toPrimitive]();
+    }
+    [(_a = exports.SYM_CONFIG, _b = exports.SYM_YARGS, Symbol.toPrimitive)]() {
+        let { command, describe, aliases } = this;
+        if (Array.isArray(command)) {
+            command = command.slice();
+        }
+        if (Array.isArray(aliases)) {
+            aliases = aliases.slice();
+        }
+        let builder = this.builder;
+        let handler = this.handler;
+        return {
+            command,
+            aliases,
+            describe,
+            builder,
+            handler,
+        };
+    }
+}
+__decorate([
+    core_decorators_1.autobind,
+    __metadata("design:type", Object),
+    __metadata("design:paramtypes", [Object])
+], YargsCommandModule.prototype, "command", null);
+__decorate([
+    core_decorators_1.autobind,
+    __metadata("design:type", Object),
+    __metadata("design:paramtypes", [Object])
+], YargsCommandModule.prototype, "describe", null);
+__decorate([
+    core_decorators_1.autobind,
+    __metadata("design:type", Object),
+    __metadata("design:paramtypes", [Object])
+], YargsCommandModule.prototype, "aliases", null);
+__decorate([
+    core_decorators_1.autobind,
+    __metadata("design:type", Object),
+    __metadata("design:paramtypes", [])
+], YargsCommandModule.prototype, "builder", null);
+__decorate([
+    core_decorators_1.autobind,
+    __metadata("design:type", Object),
+    __metadata("design:paramtypes", [])
+], YargsCommandModule.prototype, "yargs", null);
+__decorate([
+    core_decorators_1.autobind,
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [])
+], YargsCommandModule.prototype, "handler", null);
+__decorate([
+    core_decorators_1.autobind,
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [Function]),
+    __metadata("design:returntype", void 0)
+], YargsCommandModule.prototype, "setHandler", null);
+__decorate([
+    core_decorators_1.autobind,
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [Function]),
+    __metadata("design:returntype", void 0)
+], YargsCommandModule.prototype, "newHandler", null);
+__decorate([
+    core_decorators_1.autobind,
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", []),
+    __metadata("design:returntype", void 0)
+], YargsCommandModule.prototype, "toValue", null);
 exports.YargsCommandModule = YargsCommandModule;
 exports.default = YargsCommandModule;
 //# sourceMappingURL=index.js.map
