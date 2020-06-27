9d3805c1fc4608758e2682935e98d7bbfbdb56ed
bluelovers <codelovers@users.sourceforge.net>
2020-06-27 11:59:11 +0800

    build: update js

--- packages/@yarn-tool/pack-package/index.js
+++ packages/@yarn-tool/pack-package/index.js
@@ -5,11 +5,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.packPackage = exports.getPackedTarballInfo = exports.printPackedTarballInfo = exports.packTargetPackage = void 0;
 const pack_1 = require("./lib/pack");
-Object.defineProperty(exports, "packTargetPackage", { enumerable: true, get: function () { return pack_1.packTargetPackage; } });
 const info_1 = require("./lib/info");
-Object.defineProperty(exports, "getPackedTarballInfo", { enumerable: true, get: function () { return info_1.getPackedTarballInfo; } });
 const print_1 = require("./lib/print");
-Object.defineProperty(exports, "printPackedTarballInfo", { enumerable: true, get: function () { return print_1.printPackedTarballInfo; } });
 var pack_2 = require("./lib/pack");
 Object.defineProperty(exports, "packTargetPackage", { enumerable: true, get: function () { return pack_2.packTargetPackage; } });
 var print_2 = require("./lib/print");
