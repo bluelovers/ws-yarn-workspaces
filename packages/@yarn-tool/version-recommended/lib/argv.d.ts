85310cfaa357a3244d0d6c5c004c0821a3119fed
bluelovers <codelovers@users.sourceforge.net>
2020-08-03 00:34:56 +0800

    feat: setupToYargs

---
+++ packages/@yarn-tool/version-recommended/lib/argv.d.ts
@@ -0,0 +1,22 @@
+import { Argv } from 'yargs';
+export declare function setupToYargs<T>(yargs: Argv<T>): Argv<T & {
+    "no-git-tag-version": boolean;
+} & {
+    "new-version": string;
+} & {
+    major: boolean;
+} & {
+    minor: boolean;
+} & {
+    patch: boolean;
+} & {
+    premajor: boolean;
+} & {
+    preminor: boolean;
+} & {
+    prepatch: boolean;
+} & {
+    prerelease: boolean;
+} & {
+    "non-interactive": boolean;
+}>;
---
+++ packages/@yarn-tool/version-recommended/lib/argv.js
@@ -0,0 +1,49 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.setupToYargs = void 0;
+function setupToYargs(yargs) {
+    return yargs
+        .option('no-git-tag-version', {
+        desc: 'no git tag version',
+        boolean: true,
+        default: true,
+    })
+        .option('new-version', {
+        desc: 'new version',
+        string: true,
+    })
+        .option('major', {
+        desc: 'auto-increment major version number',
+        boolean: true,
+    })
+        .option('minor', {
+        desc: 'auto-increment minor version number',
+        boolean: true,
+    })
+        .option('patch', {
+        desc: 'auto-increment patch version number',
+        boolean: true,
+    })
+        .option('premajor', {
+        desc: 'auto-increment premajor version number',
+        boolean: true,
+    })
+        .option('preminor', {
+        desc: 'auto-increment preminor version number',
+        boolean: true,
+    })
+        .option('prepatch', {
+        desc: 'auto-increment prepatch version number',
+        boolean: true,
+    })
+        .option('prerelease', {
+        desc: 'auto-increment prerelease version number',
+        boolean: true,
+    })
+        .option('non-interactive', {
+        desc: 'do not show interactive prompts',
+        boolean: true,
+    });
+}
+exports.setupToYargs = setupToYargs;
+//# sourceMappingURL=argv.js.map
