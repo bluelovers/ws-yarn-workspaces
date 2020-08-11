3e1ad69d340499b272141fb7afc5be81f65f9b1d
bluelovers <codelovers@users.sourceforge.net>
2020-08-11 16:52:17 +0800

    refactor: part 3

--- packages/@lazy-node/semver-parse/test/deepOwnEqual.ts
+++ packages/@lazy-node/semver-parse/test/deepOwnEqual.ts
@@ -1,23 +1,26 @@
-function deepOwnEqual(a, b) {
+export function deepOwnEqual(a, b)
+{
 
-  // if arrays of objects, recurse down to the objects
-  if(Array.isArray(a) && Array.isArray(b)) {
-    expect(a.length).toEqual(b.length)
-    for(var i=0; i<a.length; i++) {
-      deepOwnEqual(a[i], b[i])
-    }
-  }
-  // compare all the object properties
-  else {
-    var aKeys = Object.keys(a);
-    var bKeys = Object.keys(b);
+	// if arrays of objects, recurse down to the objects
+	if (Array.isArray(a) && Array.isArray(b))
+	{
+		expect(a.length).toEqual(b.length)
+		for (let i = 0; i < a.length; i++)
+		{
+			deepOwnEqual(a[i], b[i])
+		}
+	}
+	// compare all the object properties
+	else
+	{
+		const aKeys = Object.keys(a);
+		const bKeys = Object.keys(b);
 
-    expect(aKeys).toEqual(bKeys);
+		expect(aKeys).toEqual(bKeys);
 
-    aKeys.forEach(function(key) {
-      expect(a[key]).toEqual(b[key])
-    });
-  }
+		aKeys.forEach(function (key)
+		{
+			expect(a[key]).toEqual(b[key])
+		});
+	}
 }
-
-module.exports = deepOwnEqual
--- packages/@lazy-node/semver-parse/test/test.spec.ts
+++ packages/@lazy-node/semver-parse/test/test.spec.ts
@@ -1,201 +1,249 @@
-var semverutils = require('../semver-utils');
-var deepOwnEqual = require('./deepOwnEqual');
+import { deepOwnEqual } from './deepOwnEqual';
+import * as semverutils from '..';
 
-describe('parse', function() {
+describe('parse', function ()
+{
 
-  it('should parse a simple 3-part version', function() {
-    deepOwnEqual(semverutils.parse('1.0.0'), {
-      semver: '1.0.0',
-      version: '1.0.0',
-      major: '1',
-      minor: '0',
-      patch: '0'
-    });
-  });
+	it('should parse a simple 3-part version', function ()
+	{
+		deepOwnEqual(semverutils.parse('1.0.0'), {
+			semver: '1.0.0',
+			version: '1.0.0',
+			major: '1',
+			minor: '0',
+			patch: '0',
+		});
+	});
 
-  it('should parse pre-release versions', function() {
-    deepOwnEqual(semverutils.parse('1.0.0-alpha1'), {
-      semver: '1.0.0-alpha1',
-      version: '1.0.0',
-      major: '1',
-      minor: '0',
-      patch: '0',
-      release: 'alpha1'
-    });
-  });
+	it('should parse pre-release versions', function ()
+	{
+		deepOwnEqual(semverutils.parse('1.0.0-alpha1'), {
+			semver: '1.0.0-alpha1',
+			version: '1.0.0',
+			major: '1',
+			minor: '0',
+			patch: '0',
+			release: 'alpha1',
+		});
+	});
 
-  it('should parse build numbers', function() {
-    deepOwnEqual(semverutils.parse('1.0.0+build-123'), {
-      semver: '1.0.0+build-123',
-      version: '1.0.0',
-      major: '1',
-      minor: '0',
-      patch: '0',
-      build: 'build-123'
-    });
-  });
+	it('should parse build numbers', function ()
+	{
+		deepOwnEqual(semverutils.parse('1.0.0+build-123'), {
+			semver: '1.0.0+build-123',
+			version: '1.0.0',
+			major: '1',
+			minor: '0',
+			patch: '0',
+			build: 'build-123',
+		});
+	});
 
-  it('should not parse invalid versions', function() {
-    expect(semverutils.parse('a.b.c')).toEqual(null);
-    expect(semverutils.parse('1')).toEqual(null);
-    expect(semverutils.parse('1.0')).toEqual(null);
-    expect(semverutils.parse('1.0.0b')).toEqual(null);
-    expect(semverutils.parse('1.0.0+build-abc.')).toEqual(null);
-  });
+	it('should not parse invalid versions', function ()
+	{
+		expect(semverutils.parse('a.b.c')).toEqual(null);
+		expect(semverutils.parse('1')).toEqual(null);
+		expect(semverutils.parse('1.0')).toEqual(null);
+		expect(semverutils.parse('1.0.0b')).toEqual(null);
+		expect(semverutils.parse('1.0.0+build-abc.')).toEqual(null);
+	});
 
 });
 
-describe('parseRange', function() {
+describe('parseRange', function ()
+{
 
-  it('should parse an exact version as a range', function() {
+	it('should parse an exact version as a range', function ()
+	{
 
-    deepOwnEqual(semverutils.parseRange('1.0.0'), [{
-      semver: '1.0.0',
-      major: '1',
-      minor: '0',
-      patch: '0'
-    }]);
-  });
+		deepOwnEqual(semverutils.parseRange('1.0.0'), [
+			{
+				semver: '1.0.0',
+				major: '1',
+				minor: '0',
+				patch: '0',
+			},
+		]);
+	});
 
-  it('should ignore the v- prefix', function() {
+	it('should ignore the v- prefix', function ()
+	{
 
-    deepOwnEqual(semverutils.parseRange('v1.0.0'), [{
-      semver: 'v1.0.0',
-      major: '1',
-      minor: '0',
-      patch: '0'
-    }]);
-  });
+		deepOwnEqual(semverutils.parseRange('v1.0.0'), [
+			{
+				semver: 'v1.0.0',
+				major: '1',
+				minor: '0',
+				patch: '0',
+			},
+		]);
+	});
 
-  it('should parse a comparison operator', function() {
-    deepOwnEqual(semverutils.parseRange('< v2.0.0'), [{
-      semver: '< v2.0.0',
-      operator: '<',
-      major: '2',
-      minor: '0',
-      patch: '0'
-    }]);
-  });
+	it('should parse a comparison operator', function ()
+	{
+		deepOwnEqual(semverutils.parseRange('< v2.0.0'), [
+			{
+				semver: '< v2.0.0',
+				operator: '<',
+				major: '2',
+				minor: '0',
+				patch: '0',
+			},
+		]);
+	});
 
-  it('should parse tilde', function() {
-    deepOwnEqual(semverutils.parseRange('~1.0.0'), [{
-      semver: '~1.0.0',
-      operator: '~',
-      major: '1',
-      minor: '0',
-      patch: '0'
-    }]);
-  });
+	it('should parse tilde', function ()
+	{
+		deepOwnEqual(semverutils.parseRange('~1.0.0'), [
+			{
+				semver: '~1.0.0',
+				operator: '~',
+				major: '1',
+				minor: '0',
+				patch: '0',
+			},
+		]);
+	});
 
-  it('should parse caret', function() {
-    deepOwnEqual(semverutils.parseRange('^1.0.0'), [{
-      semver: '^1.0.0',
-      operator: '^',
-      major: '1',
-      minor: '0',
-      patch: '0'
-    }]);
-  });
+	it('should parse caret', function ()
+	{
+		deepOwnEqual(semverutils.parseRange('^1.0.0'), [
+			{
+				semver: '^1.0.0',
+				operator: '^',
+				major: '1',
+				minor: '0',
+				patch: '0',
+			},
+		]);
+	});
 
-  it('should parse tilde and v- prefix', function() {
-    deepOwnEqual(semverutils.parseRange('~v1.0.0'), [{
-      semver: '~v1.0.0',
-      operator: '~',
-      major: '1',
-      minor: '0',
-      patch: '0'
-    }]);
-  });
+	it('should parse tilde and v- prefix', function ()
+	{
+		deepOwnEqual(semverutils.parseRange('~v1.0.0'), [
+			{
+				semver: '~v1.0.0',
+				operator: '~',
+				major: '1',
+				minor: '0',
+				patch: '0',
+			},
+		]);
+	});
 
-  it('should parse ||', function() {
-    deepOwnEqual(semverutils.parseRange('~1.0.0 || ~2.0.0'), [{
-      semver: '~1.0.0',
-      operator: '~',
-      major: '1',
-      minor: '0',
-      patch: '0'
-    }, {
-      operator: '||'
-    }, {
-      semver: '~2.0.0',
-      operator: '~',
-      major: '2',
-      minor: '0',
-      patch: '0'
-    }]);
-  });
+	it('should parse ||', function ()
+	{
+		deepOwnEqual(semverutils.parseRange('~1.0.0 || ~2.0.0'), [
+			{
+				semver: '~1.0.0',
+				operator: '~',
+				major: '1',
+				minor: '0',
+				patch: '0',
+			}, {
+				operator: '||',
+			}, {
+				semver: '~2.0.0',
+				operator: '~',
+				major: '2',
+				minor: '0',
+				patch: '0',
+			},
+		]);
+	});
 
-  it('should parse build numbers', function() {
-    deepOwnEqual(semverutils.parseRange('2.0.0+build.1848'), [{
-      semver: '2.0.0+build.1848',
-      major: '2',
-      minor: '0',
-      patch: '0',
-      build: 'build.1848'
-    }]);
-  });
+	it('should parse build numbers', function ()
+	{
+		deepOwnEqual(semverutils.parseRange('2.0.0+build.1848'), [
+			{
+				semver: '2.0.0+build.1848',
+				major: '2',
+				minor: '0',
+				patch: '0',
+				build: 'build.1848',
+			},
+		]);
+	});
 
-  it('should parse pre-release versions', function() {
-    deepOwnEqual(semverutils.parseRange('1.0.0-rc1'), [{
-      semver: '1.0.0-rc1',
-      major: '1',
-      minor: '0',
-      patch: '0',
-      release: 'rc1'
-    }]);
-  });
+	it('should parse pre-release versions', function ()
+	{
+		deepOwnEqual(semverutils.parseRange('1.0.0-rc1'), [
+			{
+				semver: '1.0.0-rc1',
+				major: '1',
+				minor: '0',
+				patch: '0',
+				release: 'rc1',
+			},
+		]);
+	});
 
-  it('should parse pre-release versions with hyphens', function() {
+	it('should parse pre-release versions with hyphens', function ()
+	{
 
-    deepOwnEqual(semverutils.parseRange('1.0.0-rc-2'), [{
-      semver: '1.0.0-rc-2',
-      major: '1',
-      minor: '0',
-      patch: '0',
-      release: 'rc-2'
-    }]);
-  });
+		deepOwnEqual(semverutils.parseRange('1.0.0-rc-2'), [
+			{
+				semver: '1.0.0-rc-2',
+				major: '1',
+				minor: '0',
+				patch: '0',
+				release: 'rc-2',
+			},
+		]);
+	});
 
-  it('should parse hyphen ranges', function() {
-    deepOwnEqual(semverutils.parseRange('1.0.0 - 1.0.x'), [{
-      semver: '1.0.0',
-      major: '1',
-      minor: '0',
-      patch: '0'
-    }, {
-      operator: '-'
-    }, {
-      semver: '1.0.x',
-      major: '1',
-      minor: '0',
-      patch: 'x'
-    }]);
-  });
+	it('should parse hyphen ranges', function ()
+	{
+		deepOwnEqual(semverutils.parseRange('1.0.0 - 1.0.x'), [
+			{
+				semver: '1.0.0',
+				major: '1',
+				minor: '0',
+				patch: '0',
+			}, {
+				operator: '-',
+			}, {
+				semver: '1.0.x',
+				major: '1',
+				minor: '0',
+				patch: 'x',
+			},
+		]);
+	});
 
-  it('should parse constrained * ranges', function() {
-    deepOwnEqual(semverutils.parseRange('1.*'), [{
-      semver: '1.*',
-      major: '1',
-      minor: '*',
-    }]);
-  });
+	it('should parse constrained * ranges', function ()
+	{
+		deepOwnEqual(semverutils.parseRange('1.*'), [
+			{
+				semver: '1.*',
+				major: '1',
+				minor: '*',
+			},
+		]);
+	});
 
-  it('should parse constrained .x', function() {
-    deepOwnEqual(semverutils.parseRange('1.x'), [{
-      semver: '1.x',
-      major: '1',
-      minor: 'x',
-    }]);
-  });
+	it('should parse constrained .x', function ()
+	{
+		deepOwnEqual(semverutils.parseRange('1.x'), [
+			{
+				semver: '1.x',
+				major: '1',
+				minor: 'x',
+			},
+		]);
+	});
 
-  it('should parse ~> ranges', function() {
-    deepOwnEqual(semverutils.parseRange('~> 2.0.0'), [{
-      semver: '~> 2.0.0',
-      operator: '~>',
-      major: '2',
-      minor: '0',
-      patch: '0'
-    }]);
-  });
+	it('should parse ~> ranges', function ()
+	{
+		deepOwnEqual(semverutils.parseRange('~> 2.0.0'), [
+			{
+				semver: '~> 2.0.0',
+				operator: '~>',
+				major: '2',
+				minor: '0',
+				patch: '0',
+			},
+		]);
+	});
 
 });
