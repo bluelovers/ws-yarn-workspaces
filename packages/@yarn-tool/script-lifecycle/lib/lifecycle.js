bd9c1c0288ee00ff54710b99a088dadaeff968c4
bluelovers <codelovers@users.sourceforge.net>
2021-07-16 13:31:04 +0800

    build: update js

--- packages/@yarn-tool/script-lifecycle/lib/lifecycle.js
+++ packages/@yarn-tool/script-lifecycle/lib/lifecycle.js
@@ -1,10 +1,12 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.lifecycleMap = void 0;
-const tslib_1 = require("tslib");
-const install_1 = (0, tslib_1.__importDefault)(require("./lifecycle/install"));
-const pack_1 = (0, tslib_1.__importDefault)(require("./lifecycle/pack"));
-const publish_1 = (0, tslib_1.__importDefault)(require("./lifecycle/publish"));
+const install_1 = __importDefault(require("./lifecycle/install"));
+const pack_1 = __importDefault(require("./lifecycle/pack"));
+const publish_1 = __importDefault(require("./lifecycle/publish"));
 exports.lifecycleMap = {
     install: install_1.default,
     pack: pack_1.default,
