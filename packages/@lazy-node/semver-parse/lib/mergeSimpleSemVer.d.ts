68b80888dade4eb368927afdd50066488014ecbd
bluelovers <codelovers@users.sourceforge.net>
2021-07-16 14:41:09 +0800

    feat: sort and update deps

--- packages/@lazy-node/semver-parse/lib/mergeSimpleSemVer.d.ts
+++ packages/@lazy-node/semver-parse/lib/mergeSimpleSemVer.d.ts
@@ -5,6 +5,6 @@ import { ISimpleSemVer, ISimpleSemVerObjectBase } from './types';
 import { ITSPartialPick } from 'ts-type/lib/type/record';
 export declare function mergeSimpleSemVer<T extends ISimpleSemVer>(target: T, b: ISimpleSemVerObjectBase): {
     target: import("./types").IToSimpleSemVerObject<T>;
-    changed: ITSPartialPick<ISimpleSemVerObjectBase, "major" | "minor" | "patch" | "release" | "build">;
+    changed: ITSPartialPick<ISimpleSemVerObjectBase, "release" | "major" | "minor" | "patch" | "build">;
 };
 export default mergeSimpleSemVer;
