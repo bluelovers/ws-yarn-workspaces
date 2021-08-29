8151275d06ee7bcc5ec217d82992436d0ef2a2e1
bluelovers <codelovers@users.sourceforge.net>
2021-08-29 18:01:46 +0800

    test: add test

---
+++ packages/@yarn-tool/pack-package/test/pak.spec.ts
@@ -0,0 +1,21 @@
+import { basename, dirname, extname } from 'path';
+import { packPackage } from '../index';
+import { resolvePackage } from '@yarn-tool/resolve-package/index';
+
+test(`packPackage`, async () =>
+{
+	const packageDir = resolvePackage('@yarn-tool/script-lifecycle/test/res/pkg-a').pkgRoot;
+
+	let actual = await packPackage({
+		packageDir,
+	});
+
+	expect(actual).toMatchSnapshot({
+		packageDir: expect.any(String),
+		packageTarball: expect.any(String),
+		pkg: expect.any(Object),
+	});
+
+	await actual.log();
+
+});
