c2ae3503001f9c1ed3a163a59cc4b1ea964d2de4
bluelovers <codelovers@users.sourceforge.net>
2022-08-06 08:32:48 +0800

    test: update test

--- packages/@yarn-tool/yarnlock-ncu/test/tag.spec.ts
+++ packages/@yarn-tool/yarnlock-ncu/test/tag.spec.ts
@@ -4,27 +4,63 @@ import { readFileSync } from "fs";
 import { __TEST_YARNLOCK } from '../../../../__root_ws';
 import { printReport } from '../lib/printReport';
 import stripAnsi from 'strip-ansi';
+import { pathExistsSync } from 'fs-extra';
+import { EnumDetectYarnLock } from '@yarn-tool/yarnlock-types';
+import { _forEachVersionTags } from '../../../../test/lib/forEachVersionTags';
+
+const TIMEOUT = 5 * 60 * 1000;
 
 describe(`fixYarnLockTagUpdate`, () =>
 {
-
-	test(`should update tag in yarn.lock`, async () =>
+	_forEachVersionTags().forEach(ver =>
 	{
-		let file = join(__TEST_YARNLOCK, 'ncu', 'v1', 'yarn.lock');
-		let buf = readFileSync(file);
+		describe(ver, () =>
+		{
+			const dir = join(__TEST_YARNLOCK, 'ncu', ver);
 
-		let actual = await updateYarnLockTag(buf);
+			if (!pathExistsSync(dir))
+			{
+				test(`dummy`, () => {})
+			}
+			else
+			{
+				test(`should update tag in yarn.lock`, async () =>
+				{
+					const file = join(dir, 'yarn.lock');
+					let buf = readFileSync(file);
 
-		console.dir(actual.yarnlock_changed);
+					let actual = await updateYarnLockTag(buf);
 
-		expect(actual).toHaveProperty(['yarnlock_changed'], true);
+					console.dir(actual.yarnlock_changed);
 
-		console.dir(actual.report);
+					expect(actual).toHaveProperty(['yarnlock_changed'], true);
 
-		expect(actual).toHaveProperty(['report', 'removed', 'next@canary']);
-		expect(actual).toHaveProperty(['report', 'removed', 'typescript@next']);
+					console.dir(actual.report);
 
+					expect(actual).toHaveProperty(['report', 'removed', 'next@canary']);
+					expect(actual).toHaveProperty(['report', 'removed', 'typescript@next']);
 
-	}, 5 * 60 * 1000);
+					expect(actual).toMatchSnapshot({
+						yarnlock_new: expect.any(String),
+						yarnlock_old: expect.any(String),
+						report: {
+							removed: {
+								'next@canary': {
+									to: expect.any(String),
+								},
+								'typescript@next': {
+									to: expect.any(String),
+								},
+							},
+						},
+					});
+
+					console.log(printReport(actual.report));
+
+				}, TIMEOUT);
+			}
+
+		});
+	});
 
 })
