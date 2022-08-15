b49113161099b6c5e3888c87e43abbded59b8ad9
bluelovers <codelovers@users.sourceforge.net>
2022-08-15 08:13:12 +0800

    fix: stringifyRangeSet

--- packages/@lazy-node/semver-ampersand/lib/range/stringifyRangeSet.js
+++ packages/@lazy-node/semver-ampersand/lib/range/stringifyRangeSet.js
@@ -1,8 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.stringifyRangeSet = void 0;
+const const_1 = require("../const");
 function stringifyRangeSet(rangeSet) {
-    return rangeSet.flat().join(' ');
+    return rangeSet
+        .map(a => a.join(' '))
+        .join(const_1.separatorDoubleVerticalBar);
 }
 exports.stringifyRangeSet = stringifyRangeSet;
 //# sourceMappingURL=stringifyRangeSet.js.map
--- packages/@lazy-node/semver-ampersand/lib/range/stringifyRangeSet.ts
+++ packages/@lazy-node/semver-ampersand/lib/range/stringifyRangeSet.ts
@@ -1,6 +1,9 @@
 import { buildRangeSet } from './buildRangeSet';
+import { separatorDoubleVerticalBar } from '../const';
 
 export function stringifyRangeSet(rangeSet: ReturnType<typeof buildRangeSet>)
 {
-	return rangeSet.flat().join(' ')
+	return rangeSet
+		.map(a => a.join(' '))
+		.join(separatorDoubleVerticalBar)
 }
