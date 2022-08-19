b76c5a8902781abb3bee14492e652b89307ffacd
bluelovers <codelovers@users.sourceforge.net>
2022-08-19 09:42:42 +0800

    .

--- packages/@yarn-tool/static-file/lib/const.d.ts
+++ packages/@yarn-tool/static-file/lib/const.d.ts
@@ -1,4 +1,4 @@
 import { IStaticFilesMapArray } from './types';
 export declare const defaultCopyStaticFiles: IStaticFilesMapArray<"tsconfig.json.tpl" | "test/tsconfig.json.tpl" | "tsconfig.esm.json.tpl" | "tsconfig.tsdx.json.tpl" | ".eslintrc.json.tpl" | "now.json.tpl" | ".npmrc.tpl" | "tsdx.config.js.tpl" | "tsc-multi.json.tpl">;
-export declare const defaultCopyStaticFilesRootOnly: IStaticFilesMapArray<"lerna.json.tpl" | "pnpm-workspace.yaml.tpl" | "tsconfig.json" | ".eslintrc.json" | ".yarnrc.yml.tpl">;
+export declare const defaultCopyStaticFilesRootOnly: IStaticFilesMapArray<"tsconfig.json" | ".eslintrc.json" | "lerna.json.tpl" | "pnpm-workspace.yaml.tpl" | ".yarnrc.yml.tpl">;
 export declare const defaultCopyStaticFilesWsRootOnly: IStaticFilesMapArray<"tsc-multi.json.tpl">;
